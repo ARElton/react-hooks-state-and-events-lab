e42ed240805150d7090c9ea5032f0bf0ce5aa256
ARElton <ameliareneeelton@gmail.com>
2021-01-17 17:21:58 -0500

    add state

--- src/components/App.js
+++ src/components/App.js
@@ -4,15 +4,20 @@ import itemData from "../data/items";
 
 function App() {
   const [items, setItems] = useState(itemData);
+  const [mode, setMode] = useState(false)
+
+  function handleDMClick() {
+    setMode((mode) => !mode)
+  }
 
   // this data will be passed down to the ShoppingList as a prop
   console.log(items);
 
   return (
-    <div className={"App " + (false ? "dark" : "light")}>
+    <div className={"App " + (mode ? "dark" : "light")}>
       <header>
         <h2>Shopster</h2>
-        <button>Dark Mode</button>
+        <button onClick={handleDMClick}>{mode ? "Dark" : "Light"}</button>
       </header>
       <ShoppingList items={items} />
     </div>
